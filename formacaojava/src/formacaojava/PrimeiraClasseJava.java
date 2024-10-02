ebf334550c0d01dd2441d5ac6f16495304fbbff0
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-02 20:09:40 -0300

    Módulo 7 Aula 6 - Fazendo operações entre números inteiros

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -2,23 +2,19 @@ package formacaojava;
 
 public class PrimeiraClasseJava {
 	
-	/*Variável Global é acessível a todos*/
-	static int maiorIdadeGlobal = 30;
-	
 	/*
 	 * Main é um método auto executável em Java
 	 * OBS: tudo que eu precisar executar, tenho que chamar dentro do Main
 	 * */
 	public static void main(String[] args) {
-		/*Variável local porque pertence somente a um método*/
-		int maiorIdade = 18;
-		System.out.println("valor da variável LOCAL: " + maiorIdade);
 		
-		metodo2();
-	}
-	
-	public static void metodo2() {
-		System.out.println("Valor da variável GLOBAL: " + maiorIdadeGlobal);
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		
+		System.out.println("Médias das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);
 	}
 	
 }	
