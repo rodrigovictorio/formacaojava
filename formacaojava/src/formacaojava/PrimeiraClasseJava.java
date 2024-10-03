92f4c529063cd64e377fe81f7b30074cacfc67bb
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-03 19:25:36 -0300

    Módulo 7 Aula 8 - O tipo CHAR de caracteres

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -8,15 +8,17 @@ public class PrimeiraClasseJava {
 	 * */
 	public static void main(String[] args) {
 		
-		double nota1 = 90;
-		double nota2 = 70;
-		double nota3 = 50;
-		double nota4 = 88;
+		/*
+		 * CHAT representa uma letra
+		 */
+		char pessoaMasculino = 'M';
 		
-		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+		if(pessoaMasculino == 'M') {
+			System.out.println("Masculino");
+		}else {
+			System.out.println("Feminino");
+		}
 		
-		
-		System.out.println("Médias das notas = " + mediaFinal);
 	}
 	
 }	
