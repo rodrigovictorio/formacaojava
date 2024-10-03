8cab4a38c1b52db86f3431486afc96a3e20afedf
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-03 20:18:51 -0300

    Módulo 8 Aula 1 - Operações lógicas com IF e ELSE PARTE 1

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -1,19 +1,20 @@
 package formacaojava;
 
 public class PrimeiraClasseJava {
-	
+
 	public static void main(String[] args) {
-		
-		String nome = "Rodrigo Victório";
-		String cpf = "123.456.789-99";
-		String telefone = "21 91234-5678";
-		String endereco = "Rio de Janeiro, RJ";
-		
-		System.out.println("Meu nome é " + nome 
-				+ ",portador do CPF de nº " + cpf 
-				+ ", meu telefone para contato é o " + telefone 
-				+ " e eu moro no " + endereco);
-		
+
+		int mediaAluno = 70;
+		String nome = "João";
+
+		if (mediaAluno >= 70 && nome.equals("Rodrigo")) {
+			System.out.println("Parabéns, você está aprovado!");
+		} else if (mediaAluno < 70) {
+			System.out.println("Você está reprovado!");
+		} else {
+			System.out.println("Aluno não encontrado.");
+		}
+
 	}
-	
-}	
+
+}
