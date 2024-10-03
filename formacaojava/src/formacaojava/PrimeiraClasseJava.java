19f85cd5387afedddf6ce162bc7f3f3e4d726271
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-03 19:15:46 -0300

    Módulo 7 Aula 7 - O tipo double de dados

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -8,13 +8,15 @@ public class PrimeiraClasseJava {
 	 * */
 	public static void main(String[] args) {
 		
-		int nota1 = 90;
-		int nota2 = 70;
-		int nota3 = 50;
-		int nota4 = 88;
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
 		
 		
-		System.out.println("Médias das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);
+		System.out.println("Médias das notas = " + mediaFinal);
 	}
 	
 }	
