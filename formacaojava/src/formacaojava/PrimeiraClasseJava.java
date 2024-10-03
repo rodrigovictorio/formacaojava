7c6390c9c6b40c524db2d76418a60d1260f0dfac
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-03 19:55:12 -0300

    Módulo 7 Aula 10 - Concatenação de variáveis

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -4,13 +4,15 @@ public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
 		
-		/*
-		 * STRING em Java sempre será um texto de tamanho qualquer
-		 */
-		String nome;
-		String cpf;
-		String telefone;
-		String endereco;
+		String nome = "Rodrigo Victório";
+		String cpf = "123.456.789-99";
+		String telefone = "21 91234-5678";
+		String endereco = "Rio de Janeiro, RJ";
+		
+		System.out.println("Meu nome é " + nome 
+				+ ",portador do CPF de nº " + cpf 
+				+ ", meu telefone para contato é o " + telefone 
+				+ " e eu moro no " + endereco);
 		
 	}
 	
