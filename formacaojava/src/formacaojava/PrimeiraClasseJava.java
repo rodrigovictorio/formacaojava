dc456a4a7cac3ce5686c8b1d2bf18956c1c9d529
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-03 21:59:26 -0300

    Módulo 8 Aula 7 - Estrutura de repetição FOR

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -4,21 +4,9 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/* Estrutura de repetição: WHILE */
-		int numero = 1;
-
-		while (numero <= 3) {
-			System.out.println("O número atual é " + numero + ".");
-			numero++;
+		/* Estrutura de repetição: FOR */
+		for(int numero = 1; numero <= 10; numero++) {
+			System.out.println("Número atual é " + numero + ".");
 		}
-		
-		/* Estrutura de repetição: DO WHILE */
-		int numero2 = 1;
-		
-		do {
-			System.out.println("O número atual " + numero2 + ".");
-			numero2++;
-		}while(numero2 <= 10);
 	}
-
 }
