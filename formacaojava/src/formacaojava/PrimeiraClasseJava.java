3397272983bb36ac86812bf6d56710e9dc10fc50
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-03 21:25:13 -0300

    Módulo 8 Aula 4 - O comando Switch e Case

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -4,32 +4,36 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		int nota1 = 90;
-		int nota2 = 90;
-		int nota3 = 90;
-		int nota4 = 90;
-		int media = 0;
-
-		media = (nota1 + nota2 + nota3 + nota4) / 4;
-
 		/*
-		 * Operações Lógicas Aninhadas: são operações dentro de operações
+		 * SWITCH CASE: operações extas
 		 */
-
-		if (media >= 50) {
-			if (media >= 70) {
-				if (media >= 90) {
-					System.out.println("Aluno está aprovado, parabéns!!!");
-				}else {
-					System.out.println("Aluno está aprovado.");
-				}
-			} else {
-				System.out.println("Aluno está em recuperação.");
-			}
-		}else {
-			System.out.println("Aluno reprovado.");
+		int dia = 4;
+		switch (dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda-feira");
+			break;
+		case 3:
+			System.out.println("Terça-feira");
+			break;
+		case 4:
+			System.out.println("Quarta-feira");
+			break;
+		case 5:
+			System.out.println("Quinta-feira");
+			break;
+		case 6:
+			System.out.println("Sexta-feira");
+			break;
+		case 7:
+			System.out.println("Sábado");
+			break;
+		default: System.out.println("Outro dia qualquer");
+			break;
 		}
-
+		
 	}
 
 }
