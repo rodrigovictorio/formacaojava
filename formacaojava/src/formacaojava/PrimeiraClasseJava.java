7d60bedcf3d5ff4fb5e76d62f510e488bbad9ec9
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-03 22:21:53 -0300

    Módulo 8 Aula 8 - Estrutura de repetição FOR com Break

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -4,9 +4,15 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/* Estrutura de repetição: FOR */
+		/* Estrutura de repetição: FOR com break*/
 		for(int numero = 1; numero <= 10; numero++) {
 			System.out.println("Número atual é " + numero + ".");
+			
+			if(numero == 7) {
+				System.out.println("Obaaa, encontramos o número 7");
+				System.out.println("estou parando de executar...");
+				break;
+			}
 		}
 	}
 }
