3241300e04bce84c59b95a43374ab342b5669ea2
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2024-10-08 16:42:25 -0300

    Módulo 8 Aula 10 - Divisões e resto com MOD do Java

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -4,14 +4,13 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/* Estrutura de repetição: FOR e Continue*/
-		for(int numero = 1; numero <= 10; numero++) {
-			if(numero == 3 || numero == 6 || numero == 9) {
-				System.out.println("Oba, encontrei o número " + numero);
-				continue;
-			}
-			
-			System.out.println("Processando laço de repetição");
-		}
+		/* MÓDULO: Resto da divisão (%) */
+		
+		double carro = 9;
+		double pessoa = 2;
+		double resto = carro % pessoa;
+		
+		System.out.println("Sobraram exatamente: " + resto + " carros.");
+		
 	}
 }
