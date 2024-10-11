7fd3b876c4028a3eb8601280c5aab9e909f1517c
rodrigovictorio <rodrigo.victorio@live.com>
2024-10-11 11:11:43 -0300

    Módulo 9 Aula 7 - Sobre o construtor do objeto PARTE 1

--- formacaojava/src/formacaojava/PrimeiraClasseJava.java
+++ formacaojava/src/formacaojava/PrimeiraClasseJava.java
@@ -1,10 +1,16 @@
 package formacaojava;
 
+import cursojava.classes.Aluno;
+
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		
-		
+		/* Objeto ainda não existe na memória */
+		Aluno aluno1;
+
+		/* Agora temos um objeto real na memória */
+		Aluno aluno2 = new Aluno();
+
 	}
 }
